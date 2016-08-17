cebf5de2b9fc4b47f341faad8a0cc5f346b9f37a
Jeremy Silver <jeremy@udacity.com>
2016-08-17 11:36:58 -0700

    Add blank libGDX project for CurveBall

---
+++ CurveBall/desktop/src/com/udacity/curveball/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.udacity.curveball.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.udacity.curveball.CurveBallGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CurveBallGame(), config);
+	}
+}
--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # EpicJavaDemos
+
+
+
+
+
+
+
+
+# Topics 
+
+Objects
+Inheritance
+Collections and Generics
+Files and I/O
+Threading
+Events
+Exceptions
+Recursion?
+Algorithems
+
+# Ideas
+
+Mandelbrot set
+Curveball
+Topdown spaceship
+Visible sorting
+Worker threads
